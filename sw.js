742ed78bb2ae0a2ca14deeb2f0f4081cefc72376
Caio Fábio Juvino de Queiroz <caio.queiroz@ccc.ufcg.edu.br>
2023-09-05 15:24:28 -0300

    Publish PWA

---
+++ sw.js
@@ -0,0 +1,22 @@
+self.addEventListener("install", e => {
+    console.log("Install!");
+    e.waitUntil(
+        caches.open("static").then(cache => {
+            return cache.addAll([
+                "./",
+                "./index.html",
+                "./src/master.css",
+                "./src/index.js",
+                "./images/logo192.png",
+                "./images/logo512.png"]);
+        })
+    );
+});
+
+self.addEventListener("fetch", e => {
+    e.respondWith(
+        caches.match(e.request).then(response => {
+            return response || fetch(e.request);
+        })
+    );
+});
